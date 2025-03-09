99c69bf421959647aa40ef3c6b89a700d47cf670
Nurlan199206 <user_1771@mail.ru>
2025-03-10 00:17:16 +0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.11.1"
+    }
+  }
+}
+
+provider "vsphere" {
+  user                 = "root"
+  password             = "yourpassword"
+  vsphere_server       = "192.168.200.17"
+  allow_unverified_ssl  = true
+  api_timeout          = 10
+}
+
+
+module "srv-01" {
+  source = "./vms/srv-01"
+}
+
+module "srv-02" {
+  source = "./vms/srv-02"
+}
